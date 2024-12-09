fececbd21c233f806497618b5c6aff2e4cd3c47f
Jade <78374850+JKFUNKO@users.noreply.github.com>
2024-12-09 00:10:26 -0800

    proto 2

--- index.js
+++ index.js
@@ -7,21 +7,71 @@ let murrayLevel = document.querySelector('.murray-level')
 let murrayIncrease = document.querySelector('.murray-increase')
 let parsedMurrayIncrease = parseFloat(murrayIncrease.innerHTML)
 
+let salCost = document.querySelector('.sal-cost')
+let parsedSalCost = parseFloat(salCost.innerHTML);
+let salLevel = document.querySelector('.sal-level')
+let salIncrease = document.querySelector('.sal-increase')
+let parsedSalIncrease = parseFloat(salIncrease.innerHTML)
+
+let brianCost = document.querySelector('.brian-cost')
+let parsedBrianCost = parseFloat(brianCost.innerHTML);
+let brianLevel = document.querySelector('.brian-level')
+let brianIncrease = document.querySelector('.brian-increase')
+let parsedBrianIncrease = parseFloat(brianIncrease.innerHTML)
+
 let jpc = 1;
+let jps = 0;
 
 function incrementJoe() {
     joe.innerHTML = Math.round(parsedJoe += jpc)
 }
 
 function buyMurray() {
-    if (parsedJoe >= parsedMurrayCost) {
-        parsedJoe -= parsedMurrayCost;
-        joe.innerHTML = parsedJoe;
+     if (parsedJoe >= parsedMurrayCost) {
+         joe.innerHTML = Math.round(parsedJoe -= parsedMurrayCost);
 
         murrayLevel.innerHTML ++
 
-        parsedMurrayIncrease = (parrsedMurrayIncrease * 1.03).toFixed(2)
+        parsedMurrayIncrease = parseFloat((parsedMurrayIncrease * 1.03).toFixed(2))
         murrayIncrease.innerHTML = parsedMurrayIncrease
         jpc += parsedMurrayIncrease
+
+        parsedMurrayCost *= 1.18;
+        murrayCost.innerHTML = Math.round(parsedMurrayCost)
     }
 }
+
+function buySal() {
+    if (parsedJoe >= parsedSalCost) {
+        joe.innerHTML = Math.round(parsedJoe -= parsedSalCost);
+
+        salLevel.innerHTML++
+
+        parsedSalIncrease = parseFloat((parsedSalIncrease * 1.03).toFixed(2))
+        salIncrease.innerHTML = parsedSalIncrease
+        jps += parsedSalIncrease
+
+        parsedSalCost *= 1.18;
+        salCost.innerHTML = Math.round(parsedSalCost)
+    }
+}
+
+function buyBrian() {
+    if (parsedJoe >= parsedBrianCost) {
+        joe.innerHTML = Math.round(parsedJoe -= parsedBrianCost);
+
+        brianLevel.innerHTML++
+
+        parsedBrianIncrease = parseFloat((parsedBrianIncrease * 1.03).toFixed(2))
+        brianIncrease.innerHTML = parsedBrianIncrease
+        jps += parsedBrianIncrease
+
+        parsedBrianCost *= 1.18;
+        brianCost.innerHTML = Math.round(parsedBrianCost)
+    }
+}
+
+setInterval(() => {
+    parsedJoe += jps /10
+    joe.innerHTML = Math.round(parsedJoe)
+}, 100)
